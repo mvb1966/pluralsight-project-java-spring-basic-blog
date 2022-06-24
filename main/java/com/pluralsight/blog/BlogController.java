228ac63e82f70a2bad9c4b4996db5517c6052d22
marc knokke heist <73223125+mvb1966@users.noreply.github.com>
2022-06-24 20:13:32 +0200

    Add files via upload

---
+++ main/java/com/pluralsight/blog/BlogController.java
@@ -0,0 +1,33 @@
+package com.pluralsight.blog;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
+
+@Controller
+public class BlogController {
+private PostRepository postRepository;
+
+	public BlogController(PostRepository postRepository) {
+		this.postRepository = postRepository;
+	}
+
+	@RequestMapping ("/")
+	public String listPosts(ModelMap modelMap){
+		List<Post> list= postRepository.getAllPosts();
+		modelMap.put("posts",list);
+		return "home";
+	}
+	@RequestMapping ("/post/{id}")
+	public String postDetails(@PathVariable Long id, ModelMap modelMap){
+		Post post = postRepository.findById(id);
+		modelMap.put("post", post);
+		return "post-details";
+	}
+}
